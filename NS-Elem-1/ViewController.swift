4ea5812ec082779efba8a5e8dd9e409730ec399e
Eric Hernandez <eands9@yahoo.com>
2018-12-02 08:42:49 -0600

    Initial Commit

---
+++ NS-Elem-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NS-Elem-1
+//
+//  Created by Eric Hernandez on 12/2/18.
+//  Copyright © 2018 Eric Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
